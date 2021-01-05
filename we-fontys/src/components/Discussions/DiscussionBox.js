91ebe46e86a75d68bde7d66b6a7773f5bdea8c7e
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2021-01-05 13:59:25 +0200

    Added comments, likes, username and user occupation to discussion box

--- we-fontys/src/components/Discussions/DiscussionBox.js
+++ we-fontys/src/components/Discussions/DiscussionBox.js
@@ -4,17 +4,26 @@ import account_image from "../../assets/account/account_icon_purple.png";
 
 const DiscussionBox = () => {
   return (
-    <div className="border rounded p-3 row">
-      <div className="col-md-2 ">
-        <img
-          src={account_image}
-          className={classes.discussion_box_image}
-          alt="img"
-        />
+    <div className="border rounded px-2 py-3 row">
+      <div className="col-md-3 row">
+        <div className="col-6 col-md-12">
+          <img
+            src={account_image}
+            className={classes.discussion_box_image}
+            alt="img"
+          />
+        </div>
+        <div className="col-6 col-md-12">
+          <span>Username -</span>
+          <span>Occupation</span>
+        </div>
       </div>
-      <div className="col-md-10 ">
+      <div className="col-md-9 p-0">
         <div className="d-flex justify-content-between my-2 my-md-0 ">
-          <h3>Heading</h3>
+          <h3>
+            Heading Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Voluptas, odit?
+          </h3>
           <p>
             <span className="badge badge-primary p-2">ICT</span>
           </p>
@@ -22,11 +31,19 @@ const DiscussionBox = () => {
         <p className="my-2 text-muted">
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea nesciunt
           reprehenderit animi illo atque repudiandae voluptatibus qui ab commodi
-          repellendus?
+          repellendus? Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+          Excepturi, illum?
         </p>
         <div className="d-flex justify-content-between">
           <button className="btn btn-link p-0 my-0">Read more</button>
-          <p className="my-0 text-muted"><i class="far fa-comment-alt"></i> 25 Comments</p>
+          <div className="icons my-0 text-muted">
+            <span className="mx-2">
+              <i className="far fa-comment-alt mx-1"></i> 25 Comments
+            </span>
+            <span className="mx-2">
+              <i class="far fa-heart mx-1"></i>40 Likes
+            </span>
+          </div>
         </div>
       </div>
     </div>
