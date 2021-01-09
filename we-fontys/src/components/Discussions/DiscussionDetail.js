472f13028eaa608f0846d90e7ac52e463aab53ba
Martin Staykov <49690101+TinStay@users.noreply.github.com>
2021-01-10 00:28:09 +0200

    Added a comment field in discussion detail component

--- we-fontys/src/components/Discussions/DiscussionDetail.js
+++ we-fontys/src/components/Discussions/DiscussionDetail.js
@@ -1,12 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import classes from "./Discussions.module.scss";
 import axios from "../../axios";
 import { Link, useHistory } from "react-router-dom";
+import { Form, Button, Alert } from "react-bootstrap";
+import { useAuth } from "../../Context/AuthContext";
 // import account_image from "../../assets/account/account_icon_purple.png";
 
+// DayJS
+import dayjs from "dayjs";
+
 const DiscussionDetail = (props) => {
+  const { currentUser } = useAuth();
+  const [userData, setUserData] = useState();
   const [discussionData, setDiscussionData] = useState({});
-  const history = useHistory();
+  
+  // Alert and error
+  const [alertMessage, setAlertMessage] = useState("");
+  const [show, setShow] = useState(true);
+  const [error, setError] = useState("");
+
+  const commentInputRef = useRef();
 
   // Fetch discussion data from Firebase Realtime Database
   useEffect(() => {
@@ -15,55 +28,133 @@ const DiscussionDetail = (props) => {
       .then((response) => {
         setDiscussionData(response.data);
       })
+      .then(() => {
+        // Fetch user data from Firebase
+        axios
+          .get(`/users/${currentUser.uid}.json`)
+          .then((userData) => {
+            // Set user data state
+            setUserData(userData.data);
+          })
+          .catch((err) => {
+            console.log("err", err);
+          });
+      })
       .catch((error) => {
         console.log("Error in fetching discussion data: ", error);
       });
   }, []);
+
+  const postComment = async () => {
+    // Reset error
+    setError("");
+
+    const comment = commentInputRef.current.value;
+
+    // Validate form
+    if (comment === "") {
+      setError("Comment field must not be empty");
+    } else {
+      let newDiscussionData = { ...discussionData };
+      let newCommentEntry = {
+        comment: comment,
+        userID: userData.userID,
+        authorProfileImage: userData.photoURL,
+        username: userData.username,
+      };
+
+      // Push comment to comments array
+      newDiscussionData.comments.push(newCommentEntry);
+
+      // Make a put request to update discussion comments data
+      await axios
+        .put(`/discussions/${props.match.params.id}.json`, newDiscussionData)
+        .then((response) => {
+          //   console.log("Response from put request: ", response.data);
+        })
+        .catch((error) => {
+          console.log("Error in fetching discussion data: ", error);
+        });
+
+      setAlertMessage("You successfuly posted your comment");
+
+      // Reset field
+      commentInputRef.current.value = "";
+    }
+  };
 
   return (
     <div className={classes.discussion_detail_container}>
       <Link className="text-decoration-none " to="/discuss">
         <i className="fas fa-angle-left mr-1"></i>Back
       </Link>
-      <div className=" rounded my-2 px-2 py-1 row">
-        {/* <div className="col-lg-3 row text-center">
-          <div className="col-4 col-lg-12">
+      <div className="rounded my-2 px-2 py-1 row">
+        <div className="w-100 my-2 my-md-0">
+          <h1 className="w-100 mb-2 pb-2 pr-md-0 border-bottom">
+            {discussionData.title}
+          </h1>
+          <div className="d-flex justify-content-between text-muted">
+            <div className="discussion-info">
+              <span className="mr-2">
+                <i className="far fa-heart mx-1 like-btn"></i>
+                {discussionData.likes === 1
+                  ? discussionData.likes + " like"
+                  : discussionData.likes + " likes"}
+              </span>
+              <span className="mx-3">
+                {dayjs(discussionData.datePosted).format("MMM DD, YYYY")}
+              </span>
+            </div>
+
+            <span className="">
+              <span className="badge badge-primary ml-1 px-3 py-2">
+                {discussionData.category}
+              </span>
+            </span>
+          </div>
+        </div>
+
+        <div className="initial-comment row w-100 my-4">
+          <div className="col-md-2 text-center">
             <img
               src={discussionData.authorProfileImage}
               className={classes.discussion_box_image}
               alt="img"
             />
           </div>
-          <div className="col-8 col-lg-12 ">
-            <p className="my-1 h5">{discussionData.author}</p>
-            <p className="my-1">{discussionData.authorStudyProgram}</p>
+          <div className="col-md-10">
+            <p className="my-2 text-muted">{discussionData.initialComment}</p>
           </div>
-        </div> */}
-        <div className="row my-2 my-md-0 ">
-          <h1 className="col-md-12 border-bottom pr-md-0">
-            {discussionData.title}
-          </h1>
-          <p className="col-md-3">
-            <span className="badge badge-primary ml-1 px-3 py-2">
-              {discussionData.category}
-            </span>
-          </p>
-          <p>
-            <span class="date">{discussionData.datePosted}</span>
-          </p>
         </div>
-        <p className="my-2 text-muted">{discussionData.initialComment}</p>
-        <div className="d-flex justify-content-end">
-          <div className="icons my-0 text-muted">
-            <span className="mx-2">
-              <i className="far fa-comment-alt mx-1"></i>
-              {discussionData.comments}
-            </span>
-            <span className="mx-2">
-              <i className="far fa-heart mx-1"></i>
-              {discussionData.likes}
-            </span>
+
+        <div className="comment-form row w-100 my-4">
+        {(alertMessage !== "" && show) ? <Alert className="col-12" variant="success" onClose={() => setShow(false)} dismissible>{alertMessage}</Alert> : null}
+        {error !== "" && <Alert className="col-12" variant="danger ">{error}</Alert>}
+          <div className="col-md-2 text-center">
+            <img
+              src={userData && userData.photoURL}
+              className={classes.discussion_box_image}
+              alt="img"
+            />
           </div>
+          <div className="col-md-10">
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Label>Comment</Form.Label>
+              <Form.Control ref={commentInputRef} as="textarea" rows={3} />
+            </Form.Group>
+            <Button onClick={postComment} variant="primary">
+              Submit
+            </Button>
+          </div>
+        </div>
+
+        <div className="comments my-5 pb-2 border-bottom w-100 ">
+          <span className="text-muted ">
+            <i className="far fa-comment-alt mx-1"></i>
+            {discussionData.commentNumber === 1
+              ? discussionData.commentNumber + " comment"
+              : discussionData.commentNumber + " comments"}
+          </span>
         </div>
       </div>
     </div>
